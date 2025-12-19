883e5148d428d575caa20cae6083ab9aa95a8252
Lio Screti <e273161@edu.sbl.ch>
2025-12-19 17:37:47 +0000

    javascript

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-let counter = 0;
+// Erstellt eine Variable mit Namen count und dem Wert 0
+let count = 0;
 
-function createNewElement() {
-  let newElement = document.createElementNS("p");
+// Erstellt eine Funktion mit dem Namen increaseCount
+function increaseCount() {
+  // Erstellt eine neue Variable mit Namen titleElement und speichert das h1-Element darin
+  let titleElement = document.querySelector("h1");
 
-  counter = counter + 1;
+  // Erhöht die Variable count um 1
+  count = count + 1;
 
-  newElement.textContent = "Elemnt " + counter;
-
-  let mainElement = document.querySelector("main");
-  mainElement.appendChild(newElement);
+  // setzt den Textinhalt von titleElement auf den Wert von count.
+  titleElement.textContent = count;
 }
-
-//bhff
