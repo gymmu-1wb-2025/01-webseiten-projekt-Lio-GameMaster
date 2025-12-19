46c9e3cccb06b2277caed1beb6f4f63ac2325504
Lio Screti <e273161@edu.sbl.ch>
2025-12-19 19:23:28 +0000

    Kommentare

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-// Erstellt eine Variable mit Namen count und dem Wert 0
+// Erstellt einen Counter mit Anfangswert 0
 let count = 0;
 
 // Erstellt eine Funktion mit dem Namen increaseCount
 function increaseCount() {
-  // Erstellt eine neue Variable mit Namen titleElement und speichert das h1-Element darin
+  // Erstellt eine neue Variable mit Namen titleElement und speichert das Titel-Element (h1) darin
   let titleElement = document.querySelector("h1");
 
-  // Erhöht die Variable count um 1
+  // Erhöht die Zahl um 1
   count = count + 1;
 
   // setzt den Textinhalt von titleElement auf den Wert von count.
